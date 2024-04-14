5dcf34e445d1beab697783ddc524d0fce01f8350
pearmeow <perryhuang2015@gmail.com>
2024-04-13 23:52:06 -0400

    fixes

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -1,26 +1,13 @@
-#! bin/bash
+#! /bin/bash
 
 #yes, duplicate curl installation if this script is run by itself
-sudo apt-get install ninja-build gettext cmake unzip curl build-essential libstdc++-12-dev
+sudo pacman -S ninja-build gettext cmake unzip curl build-essential libstdc++-12-dev
 
 git clone https://github.com/neovim/neovim.git ~/
 cd ~/neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
-cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
-cd ~/neovim && make clean
+make install
 
 #kickstart setup + treesitter + nodes
-sudo apt install make gcc ripgrep unzip
-sudo apt install npm
-sudo npm install -g tree-sitter-cli
-export NVM_DIR="$HOME/.nvm" && (
-  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
-  cd "$NVM_DIR"
-  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-) && \. "$NVM_DIR/nvm.sh"
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-sudo nvm install node
-
+sudo pacman -S make gcc ripgrep unzip
 git clone git@github.com:PearMeow/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 source ~/.zshrc
--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
 
 #get font - for windows terminal gotta change it manually
-sudo mkdir -p /usr/share/fonts/truetype/dejavu/
-sudo cp ~/settings/fonts/DejaVuSansMNerdFontMono-Regular.ttf /usr/share/fonts/truetype/dejavu/
+sudo cp ~/settings/fonts/DejaVuSansMNerdFontMono-Regular.ttf /usr/share/fonts/
 sudo fc-cache -f -v
 
 #zsh
-sudo apt install zsh fzf curl
+sudo pacman -S zsh fzf curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/Aloxaf/fzf-tab ~/.oh-my-zsh/custom/plugins/fzf-tab
