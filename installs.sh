eec085236ad1e705220abab555fe8ed3b083b109
pearmeow <perryhuang2015@gmail.com>
2024-05-21 01:53:24 +0000

    new packages

---
+++ installs.sh
@@ -0,0 +1,15 @@
+sudo pacman -S git
+sudo pacman -S xorg-server xorg-apps xorg-xinit xorg-twm xorg-xclock xterm
+sudo pacman -S xmonad xmonad-contrib
+sudo pacman -S sddm
+sudo systemctl enable sddm.service
+sudo pacman -S alacritty
+sudo pacman -S qutebrowser
+sudo pacman -S dmenu
+sudo pacman -S gvim
+sudo pacman -S make gcc ripgrep unzip
+sudo pacman -S neovim
+sudo pacman -S openssh
+sudo pacman -S fastfetch bat
+sudo pacman -S lsd
+sudo pacman -S man
