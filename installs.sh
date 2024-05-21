a395ede5390160d037c589ea58a3d1bd4d57066c
pearmeow <perryhuang2015@gmail.com>
2024-05-21 02:21:15 +0000

    packages

--- installs.sh
+++ installs.sh
@@ -1,15 +1,11 @@
-sudo pacman -S git
+# !/bin/bash
 sudo pacman -S xorg-server xorg-apps xorg-xinit xorg-twm xorg-xclock xterm
-sudo pacman -S xmonad xmonad-contrib
-sudo pacman -S sddm
+sudo pacman -S git
+
+sudo pacman -S make gcc ripgrep unzip wget fd
+sudo pacman -S --needed xmonad xmonad-contrib xmobar qutebrowser alacritty thunar gvim neovim picom dunst sddm dmenu polkit-kde-agent flameshot feh ncmpcpp btop clipmenu
 sudo systemctl enable sddm.service
-sudo pacman -S alacritty
-sudo pacman -S qutebrowser
-sudo pacman -S dmenu
-sudo pacman -S gvim
-sudo pacman -S make gcc ripgrep unzip
-sudo pacman -S neovim
-sudo pacman -S openssh
-sudo pacman -S fastfetch bat
-sudo pacman -S lsd
-sudo pacman -S man
+systemctl --user enable clipmenud.service
+
+sudo pacman -S openssh man
+sudo pacman -S fastfetch bat lsd
