5be9a16cc789bc490bbf48316f496f3891eb0bc9
pearmeow <perryhuang2015@gmail.com>
2024-05-21 09:53:52 -0400

    new stuff

--- scripts/installs.sh
+++ scripts/installs.sh
@@ -1,11 +1,16 @@
 # !/bin/bash
-sudo pacman -S xorg-server xorg-apps xorg-xinit xorg-twm xorg-xclock xterm
-sudo pacman -S git
 
-sudo pacman -S make gcc ripgrep unzip wget fd
+# Display server
+sudo pacman -S xorg-server xorg-apps xorg-xinit xorg-twm xorg-xclock xterm
+
+# Essentials
+sudo pacman -S git make gcc ripgrep unzip wget fd openssh man fastfetch bat lsd
 sudo pacman -S --needed xmonad xmonad-contrib xmobar qutebrowser alacritty thunar gvim neovim picom dunst sddm dmenu polkit-kde-agent flameshot feh ncmpcpp btop clipmenu
 sudo systemctl enable sddm.service
 systemctl --user enable clipmenud.service
+feh --bg-scale dotfiles/themes/120_-_KnFPX73.jpg
 
-sudo pacman -S openssh man
-sudo pacman -S fastfetch bat lsd
+# time stuff
+sudo timedatectl set-ntp true
+sudo timedatectl set-timezone US/Eastern
+sudo hwclock --systohc --utc
