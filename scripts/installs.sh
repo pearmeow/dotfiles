5b2024d762714f631cba37ae4695debdd083ed7e
pearmeow <perryhuang2015@gmail.com>
2024-05-22 10:34:14 -0400

    vacation

--- scripts/installs.sh
+++ scripts/installs.sh
@@ -1,17 +1,33 @@
 # !/bin/bash
 
+# Before you install
+# Make sure yay is installed from the git repo
+
 # Display server
-sudo pacman -S xorg-server xorg-apps xorg-xinit xorg-twm xorg-xclock xterm
+sudo pacman -S xorg-server xorg-apps xorg-xinit xorg-twm xorg-xclock xterm xorg-xmessage
 
 # Essentials
 sudo pacman -S git make gcc ripgrep unzip wget fd openssh man fastfetch bat lsd
-sudo pacman -S --needed xmonad xmonad-contrib xmobar qutebrowser alacritty thunar gvim neovim picom dunst sddm dmenu polkit-kde-agent flameshot feh ncmpcpp btop clipmenu
+sudo pacman -S --needed xmonad xmonad-contrib xmobar qutebrowser discord alacritty thunar gvim neovim picom dunst sddm dmenu polkit-kde-agent flameshot feh ncmpcpp btop clipmenu rofi
 sudo systemctl enable sddm.service
 systemctl --user enable clipmenud.service
-feh --bg-scale dotfiles/themes/120_-_KnFPX73.jpg
+feh --bg-scale ~/dotfiles/themes/120_-_KnFPX73.jpg
 
-# time stuff
+# Time
 sudo timedatectl set-ntp true
 sudo timedatectl set-timezone US/Eastern
 sudo hwclock --systohc --utc
-sudo pacman -S discord
+
+# Audio
+sudo pacman -S alsa-utils pipewire-alsa pipewire-pulse
+systemctl --user enable wireplumber.service
+
+# Lock
+sudo pacman -S xss-lock i3lock
+
+# Fonts
+sudo pacman -S noto-fonts-cjk noto-fonts-emoji noto-fonts ttf-jetbrains-mono-nerd
+
+# AUR (yay)
+# Backlight
+yay -S light
