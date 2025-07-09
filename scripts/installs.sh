e17b91f5d89bbbd8c05a748342e0a2f6ba56f675
pearmeow <perryhuang2015@gmail.com>
2025-07-09 14:46:39 -0400

    Change install script to be more descriptive about packages

--- scripts/installs.sh
+++ scripts/installs.sh
@@ -7,10 +7,57 @@
 sudo pacman -S --needed xorg-server xorg-apps xorg-xinit xorg-twm xorg-xclock xterm xorg-xmessage
 
 # Essentials
-sudo pacman -S --needed make gcc ripgrep unzip wget fd openssh man fastfetch bat lsd
-sudo pacman -S --needed xmonad xmonad-contrib xmobar alacritty thunar gvim neovim picom dunst sddm dmenu polkit-kde-agent flameshot feh ncmpcpp btop clipmenu rofi vi unclutter
+sudo pacman -S --needed make gcc unzip wget fd openssh man
+
+# Terminal emulator
+sudo pacman -S --needed alacritty
+polkit-kde-agent flameshot ncmpcpp btop vi
+
+# File manager
+sudo pacman -S --needed nautilus
+
+# Text editor
+sudo pacman -S --needed neovim
+
+# Compositor if not on Wayland
+sudo pacman -S --needed picom
+
+# Display manager
+sudo pacman -S --needed sddm
+
+# Polkit authentication
+sudo pacman -S --needed polkit-kde-agent
+
+# Screenshot
+sudo pacman -S --needed flameshot
+
+# Music player
+sudo pacman -S --needed ncmpcpp
+
+# System overview
+sudo pacman -S --needed btop
+
+# Replacement for ls, cat, neofetch, and grep
+sudo pacman -S --needed lsd, bat, fastfetch, ripgrep
+
+# Window manager + notification daemon + bar + background image
+sudo pacman -S --needed awesome
+
+# Hide cursor after some time
+sudo pacman -S --needed unclutter
+
+# Dmenu replacement to search for apps
+sudo pacman -S --needed rofi
+
+# Clipboard
+sudo pacman -S --needed xsel
+
+# Notification daemon if not using awesome
+# sudo pacman -S --needed dunst
+# Wallpaper if not using awesome
+# sudo pacman -S --needed feh
+
 sudo systemctl enable sddm.service
-systemctl --user enable clipmenud.service
 feh --bg-scale ~/dotfiles/wallpapers/katana.jpg
 
 # Time
@@ -19,7 +66,7 @@ sudo timedatectl set-timezone US/Eastern
 sudo hwclock --systohc --utc
 
 # Audio
-sudo pacman -S --needed alsa-utils pipewire-alsa pipewire-pulse mpd ncmpcpp
+sudo pacman -S --needed pipewire-alsa pipewire-pulse
 systemctl --user enable wireplumber.service
 
 # Lock
@@ -34,5 +81,9 @@ sudo pacman -S realtime-privileges
 
 # AUR (yay)
 # Backlight
-yay -S light brave-bin batsignal ttf-times-new-roman vesktop
+yay -S light
+# Browser, discord, and font
+yay -S brave-bin vesktop ttf-times-new-roman 
+# Signal for low battery
+yay -S batsignal
 systemctl --user enable batsignal.service
