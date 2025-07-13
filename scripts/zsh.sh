30a5908f851e185862cd522ee3037533a6a1ea18
Perry <perryhuang2015@gmail.com>
2025-07-13 10:29:35 -0400

    Make zsh script run unattended

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -3,9 +3,9 @@
 # Oh my zsh installation & dependencies
 sudo pacman -S --needed zsh fzf curl
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
 # Plugins for oh my zsh
 git clone https://github.com/Aloxaf/fzf-tab ~/.oh-my-zsh/custom/plugins/fzf-tab
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-
-sh -c $(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)
