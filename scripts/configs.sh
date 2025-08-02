54f4b1b8cff17a8ede253903d21cdbba1a9588af
Perry Huang <perryhuang2015@gmail.com>
2025-08-02 18:18:47 -0400

    feat: add gitmessage, update scripts & gitconfig

--- scripts/configs.sh
+++ scripts/configs.sh
@@ -11,6 +11,7 @@ cp ../config/home/zshrc ~/.zshrc
 cp ../config/home/clang-format ~/.clang-format
 cp ../config/home/gitignore_global ~/.gitignore_global
 cp ../config/home/gitconfig ~/.gitconfig
+cp ../config/home/gitmessage ~/.gitmessage
 cp -r ../config/home/templates ~/
 
 # Copy configs that live in .config into their respective places
