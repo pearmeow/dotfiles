2c4807fb0a65a1e676dd3a5917f755d89fcc03f6
Perry Huang <perryhuang2015@gmail.com>
2025-08-07 22:53:12 -0400

    feat: remove user hardcoding and add zsa group

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -1,6 +1,8 @@
 # Miscellaneous commands that don't belong in the other scripts
 
 # Add groups to user: replace with your own username
-sudo gpasswd -a pearmeow mpd
-sudo gpasswd -a pearmeow realtime
-sudo gpasswd -a pearmeow video
+sudo gpasswd -a $USER mpd
+sudo gpasswd -a $USER realtime
+sudo gpasswd -a $USER video
+sudo groupadd plugdev
+sudo usermod -aG plugdev $USER
