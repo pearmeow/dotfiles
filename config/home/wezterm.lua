e929ab4121d2339ad12c1a8b47736139478752e4
Perry Huang <perryhuang2015@gmail.com>
2025-08-11 23:31:29 -0400

    feat: add wezterm config to home

---
+++ config/home/wezterm.lua
@@ -0,0 +1,28 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+config.font = wezterm.font("JetBrainsMonoNerdFontMono")
+
+-- or, changing the font size and color scheme.
+config.font_size = 19
+config.color_scheme = "Dracula (Official)"
+config.max_fps = 120
+config.animation_fps = 120
+config.enable_tab_bar = false
+config.window_padding = {
+	left = 3,
+	right = 3,
+	top = 0,
+	bottom = 0,
+}
+
+-- Finally, return the configuration to wezterm:
+return config
