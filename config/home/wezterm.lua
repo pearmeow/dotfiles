f98d189a0dceae5cca1b10bb45296ca165682526
Perry Huang <perryhuang2015@gmail.com>
2025-08-15 12:10:53 -0400

    chore: change name of font

--- config/home/wezterm.lua
+++ config/home/wezterm.lua
@@ -9,7 +9,7 @@ local config = wezterm.config_builder()
 -- For example, changing the initial geometry for new windows:
 config.initial_cols = 120
 config.initial_rows = 28
-config.font = wezterm.font("JetBrainsMonoNerdFontMono")
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 -- or, changing the font size and color scheme.
 config.font_size = 19
 config.color_scheme = "Dracula (Official)"
